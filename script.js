946fdc375498c5b5090951db9585082e9258517b
sonalsuri <157942854+sonalsuri@users.noreply.github.com>
2025-01-25 13:52:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth Scrolling for Anchor Links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      target.scrollIntoView({
+        behavior: 'smooth',
+      });
+    });
+  });
+  
